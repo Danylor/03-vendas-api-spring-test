692ec48af5cc1e025e65b4a9c462cbfd552e455c
Danylo <danyloribeiro12@gmail.com>
2026-02-02 15:33:51 -0300

    Alterações diversas
    Signed-off-by: Danylo <danyloribeiro12@gmail.com>

--- src/test/java/io/github/manoelcampos/vendas/api/feature/cliente/ClienteServiceTest.java
+++ src/test/java/io/github/manoelcampos/vendas/api/feature/cliente/ClienteServiceTest.java
@@ -9,7 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class ClienteServiceTest {
@@ -26,7 +26,14 @@ class ClienteServiceTest {
         Optional<Cliente> optional = Optional.of(new Cliente());
         Mockito.when(repository.findByCpf(cpfSemSimolos))
                 .thenReturn(optional);
-        assertTrue(service.findByCpf(cpfComSimolos).isPresent());
+        //assertTrue(service.findByCpf(cpfComSimolos).isPresent());
+        //assertTrue(service.findByCpf(cpfSemSimolos).isPresent());
         //Mockito.verify(repository).findByCpf(cpf);
+
+        var cliente1 = service.findByCpf(cpfComSimolos).orElseThrow();
+        var cliente2 = service.findByCpf(cpfSemSimolos).orElseThrow();
+
+        assertEquals(cpfSemSimolos, cliente1.getCpf());
+        assertEquals(cpfSemSimolos, cliente2.getCpf());
     }
 }
